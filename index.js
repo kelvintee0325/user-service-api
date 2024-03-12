93a9765dd316b058f901dfdcd32fe3f44d94b329
hklim86 <hklim@idesion.com>
2024-03-12 15:34:22 +0800

    Delete Container

--- index.js
+++ index.js
@@ -110,6 +110,43 @@ app.put("/docker/port/:port/stop", function (req, res) {
     // });
 })
 
+app.delete("/docker/port/:port/remove", function (req, res) { 
+    let port = req.params.port;
+ 
+    docker.listContainers({ all: true }, (err, containers) => {
+        if (err) {
+            return res.json(err);
+        }
+
+        const container_info = containers.find(c => c.Names.includes('/superchat-' + port.toString()));
+ 
+        if (!container_info) {
+            return res.status(400).send({ Message: 'Container is not available.' });
+        }
+ 
+        const container_id = container_info.Id;
+
+        const container = docker.getContainer(container_id);
+
+        container.remove(function (remove_err) {
+            if (remove_err) {
+                return res.status(400).send({ Message: stop_err.toString() }); 
+            } else { 
+                return res.status(204).send({ Message: 'The resource was successfully deleted.' }); 
+            }
+        });
+    });
+
+    // exec('docker run --name user-api-7 -d -p 8080:3000 user-service-api:latest', (error, stdout, stderr) => {
+    //     if (error) {
+    //         console.error(`exec error: ${error}`);
+    //         return;
+    //     }
+    //     console.log(`stdout: ${stdout}`);
+    //     console.error(`stderr: ${stderr}`);
+    // });
+})
+
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
 })
